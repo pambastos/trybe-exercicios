fa743d7b1b494d0a0c618be4b5b4f5bf8a8fcbb9
pam <panpbastos@gmail.com>
2022-03-08 15:59:13 -0300

    primeirocommit

---
+++ fundamentos/bloco-04-introdução-js/dia-01-js/scripts.js
@@ -0,0 +1,18 @@
+const a = 5;
+const b = 10;
+
+ console.log(a + b);
+ console.log(a-b);
+ console.log(a * b);
+ console.log(a / b);
+ console.log(a % b);
+
+ const c = 10
+ const d = 12
+ 
+ if(c > d ) {
+     console.log(c)
+ }
+ else {
+     console.log(d)
+ }

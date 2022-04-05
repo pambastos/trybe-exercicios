0f2ade068de8b3a889b822d77ca2e0d1895a8baf
pam <panpbastos@gmail.com>
2022-04-05 13:19:12 -0300

    resolução-exercicio

---
+++ fundamentos/bloco-04-javascript-e-logica/aula-4.1/script.js
@@ -0,0 +1,9 @@
+const myName = "Pâmela";
+const birthCity = "São Gonçalo";
+let birtyYear = 1995;
+console.log(myName , birthCity , birtyYear);
+birtyYear = 2030;
+console.log(birtyYear);
+birthCity = "Rio de Janeiro";
+console.log(birthCity);
+// erro: constantes não podem ser reatribuidas

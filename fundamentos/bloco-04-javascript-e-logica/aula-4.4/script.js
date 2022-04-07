cc4de25050c3875fcca2cbc36a095040bbf62cbf
pam <panpbastos@gmail.com>
2022-04-07 19:40:59 -0300

    alterando

--- fundamentos/bloco-04-javascript-e-logica/aula-4.4/script.js
+++ fundamentos/bloco-04-javascript-e-logica/aula-4.4/script.js
@@ -260,64 +260,64 @@
 
 // EXERCÍCIOS 
 // 1- 
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-};
-for(let index in info){
-  console.log('Bem vinda,' + info.personagem)
-}
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// };
+// for(let index in info){
+//   console.log('Bem vinda,' + info.personagem)
+// }
 
-// 2 - 
-info.reccorente = 'sim';
-console.log(info);
+// // 2 - 
+// info.reccorente = 'sim';
+// console.log(info);
 
-// 3- 
-for(let chave in info){
-  console.log(chave)
-}
+// // 3- 
+// for(let chave in info){
+//   console.log(chave)
+// }
 
-// 4- 
-for(let chave in info){
-  console.log(info[chave])
-}
+// // 4- 
+// for(let chave in info){
+//   console.log(info[chave])
+// }
 
-// 5- 
-
-
-
-
-// 6-
-let leitor = {
-  nome: 'Julia',
-  sobrenome: 'Pessoa',
-  idade: 21,
-  livrosFavoritos: [
-    {
-      titulo: 'O Pior Dia de Todos',
-      autor: 'Daniela Kopsch',
-      editora: 'Tordesilhas',
-    },
-  ],
-};
-console.log('O livro favorito de ' + leitor.nome + leitor.sobrenome + "se chama" + leitor.livrosFavoritos[0].titulo);
-
-// 7- 
-leitor.livrosFavoritos.push({
-  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-  autor: 'JK Rowling',
-  editora: 'Rocco',
-});
-console.log(leitor)
-
-// 8- 
-console.log(leitor.nome + "tem" + leitor.livrosFavoritos.length + 'livros favoritos')
+// // 5- 
 
 
 
-// FUNÇÕES 
-function verificaSeEpalindromo (string) {
+
+// // 6-
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//   ],
+// };
+// console.log('O livro favorito de ' + leitor.nome + leitor.sobrenome + "se chama" + leitor.livrosFavoritos[0].titulo);
+
+// // 7- 
+// leitor.livrosFavoritos.push({
+//   titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//   autor: 'JK Rowling',
+//   editora: 'Rocco',
+// });
+// console.log(leitor)
+
+// // 8- 
+// console.log(leitor.nome + "tem" + leitor.livrosFavoritos.length + 'livros favoritos')
+
+
+
+// // FUNÇÕES 
+// function verificaSeEpalindromo (string) {
  
-  
-}
+
+// }

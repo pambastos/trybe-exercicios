56e07ff1be760a7d37da291bcc61831f5bd6cbcb
pam <panpbastos@gmail.com>
2022-04-08 01:57:02 -0300

    exercicios

---
+++ fundamentos/bloco-07-javascript-es6-testes-unitarios/aula-7.1/script.js
@@ -0,0 +1,44 @@
+// Aprendendo sobre template literals ( forma mais moderna de concatenação de strings) (shift + crase + espaço)
+
+// let cliente = "Manolo";
+// let valorASerPago = 100;
+
+// const email = `Olá ${cliente} 
+// O valor da sua conta é de r$ ${valorASerPago}!
+// Se não pagar, vai ficar com o nome sujo!
+// vlw`;
+// console.log(email);
+
+// let nome = "Pâm Walsh";
+// let idade = 27;
+// console.log(`${nome} tem ${idade} anos de inferno!!!`);
+
+
+// Aprendendo sobre operador ternário 
+// Precisa começar com uma condição, algo que retorne VERDADEIRO OU FALSO. 
+// ? 
+// execução 
+// : segunda execução 
+
+// ex : 
+
+// let numero = 13;
+// let resultado = 0;
+// if(numero %2 === 0){
+//  resultado = "Número par"
+// }
+// else {
+//     resultado = "Número ímpar"
+// }
+// console.log(resultado);  
+
+
+// utilizando o operador ternário : 
+
+// let numero = 13;
+// let resultado = 0;
+// numero % 2 === 0 ? resultado = "Número par" : resultado = "Número ímpar"; 
+// console.log(resultado);
+
+// Quando usar? 
+// Quando tivermos um if else comum, como o do exemplo. 

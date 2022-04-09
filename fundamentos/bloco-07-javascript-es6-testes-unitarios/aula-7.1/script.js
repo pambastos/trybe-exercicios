acd254b17270c085341105a646c303355044df46
pam <panpbastos@gmail.com>
2022-04-08 22:25:25 -0300

    finalizando

--- fundamentos/bloco-07-javascript-es6-testes-unitarios/aula-7.1/script.js
+++ fundamentos/bloco-07-javascript-es6-testes-unitarios/aula-7.1/script.js
@@ -3,7 +3,7 @@
 // let cliente = "Manolo";
 // let valorASerPago = 100;
 
-// const email = `Olá ${cliente} 
+// const email = `Olá ${cliente}
 // O valor da sua conta é de r$ ${valorASerPago}!
 // Se não pagar, vai ficar com o nome sujo!
 // vlw`;
@@ -13,14 +13,13 @@
 // let idade = 27;
 // console.log(`${nome} tem ${idade} anos de inferno!!!`);
 
+// Aprendendo sobre operador ternário
+// Precisa começar com uma condição, algo que retorne VERDADEIRO OU FALSO.
+// ?
+// execução
+// : segunda execução
 
-// Aprendendo sobre operador ternário 
-// Precisa começar com uma condição, algo que retorne VERDADEIRO OU FALSO. 
-// ? 
-// execução 
-// : segunda execução 
-
-// ex : 
+// ex :
 
 // let numero = 13;
 // let resultado = 0;
@@ -30,15 +29,59 @@
 // else {
 //     resultado = "Número ímpar"
 // }
-// console.log(resultado);  
+// console.log(resultado);
 
-
-// utilizando o operador ternário : 
+// utilizando o operador ternário :
 
 // let numero = 13;
 // let resultado = 0;
-// numero % 2 === 0 ? resultado = "Número par" : resultado = "Número ímpar"; 
+// numero % 2 === 0 ? resultado = "Número par" : resultado = "Número ímpar";
 // console.log(resultado);
 
-// Quando usar? 
-// Quando tivermos um if else comum, como o do exemplo. 
+// Quando usar?
+// Quando tivermos um if else comum, como o do exemplo.
+
+// Arrow functions
+
+// função tradicional :
+// function divisaoTradicional (n1, n2){
+// return n1 / n2;
+// }
+// console.log(divisaoTradicional(10,2));
+
+// FUNÇÃO NÃO NOMEADA :
+// const divisaoSemNome = function (n1, n2){
+//     return n1 / n2
+// }
+// console.log(divisaoSemNome(10,2));
+
+// ARROW FUNCTION :
+// const divisao = (n1 , n2) => {
+//     return n1 / n2;
+// }
+// console.log(divisao(10,2));
+
+// CASO A FUNÇÃO TENHA APENAS UMA LINHA E RETORNE O RESULTADO DESSA LINHA, ELA PODE SER ESCRITA ASSIM:
+// const divisao = (n1, n2) => n1 / n2;
+// console.log(divisao(10,2));
+
+// const nomeCompleto = (nome, sobrenome) => `Nome completo: ${nome} ${sobrenome}`;
+// console.log(nomeCompleto("Pâm", "Walsh"));
+
+// Onde usamos arrow function?
+
+// No addEventListener :
+
+// window.onload = () => {
+// let button = document.getElementById('button');
+// button.addEventListener('click' , () => {
+//     alert('HAHAHA você não disse a palavra mágica!')
+// });
+// };
+
+
+
+
+
+
+
